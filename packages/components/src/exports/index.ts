6ea65a153c82ec4913ccfe7d0e17f72197cb9822
Franck Cornu <franck.cornu@pwc.com>
2024-11-12 08:57:26 -0500

    - Updated to MGT 4.3.0 - Added @mgt/components as export so MGT components can be used alongside search components if desired. Importing from external package won't work otherwise.

--- packages/components/src/exports/index.ts
+++ packages/components/src/exports/index.ts
@@ -9,7 +9,12 @@ export { VideoPlayerComponent } from "../components/video-player/VideoPlayerComp
 // Needed to work with SPFx as we can't use the default mgt-spfx package
 export * from "@microsoft/mgt-sharepoint-provider";
 export * from "@microsoft/mgt-teamsfx-provider";
+export * from "@microsoft/mgt-msal2-provider";
+
 export * from "@microsoft/mgt-element/dist/es6/utils/TemplateHelper";
 export * from "@microsoft/mgt-element/dist/es6/providers/IProvider";
 export * from "@microsoft/mgt-element/dist/es6/providers/Providers";
 export * from "@microsoft/mgt-element/dist/es6/utils/LocalizationHelper";
+
+// To be able to use MGT components alongside search components
+export * from "@microsoft/mgt-components";
