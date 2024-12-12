c085687ef9ce7cc36905b095201e2065c2844132
Franck Cornu <franck.cornu@outlook.com>
2024-12-12 18:25:03 -0500

    - Fixed SSR issue

--- samples/docusaurus/src/theme/SearchBar.tsx
+++ samples/docusaurus/src/theme/SearchBar.tsx
@@ -2,6 +2,7 @@ import { wrapWc } from 'wc-react';
 import { MgtLogin, SearchInputComponent } from '@pnp/modern-search-core';
 import { PageOpenBehavior, QueryPathBehavior } from '@pnp/modern-search-core/dist/es6/helpers/UrlHelper';
 import React from "react";
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const SearchInputWebComponent = wrapWc<Partial<SearchInputComponent>>('pnp-search-input');
 const MgtLoginWebComponent = wrapWc<Partial<MgtLogin>>('mgt-login');
@@ -9,18 +10,23 @@ const MgtLoginWebComponent = wrapWc<Partial<MgtLogin>>('mgt-login');
 export default function SearchBar() {
 
     return  <div className='flex items-center space-x-3'>
+                <BrowserOnly>
+                {() => {
+                    return  <>
+                                <SearchInputWebComponent 
+                                    id='f566fb98-4010-485b-a44b-b53c16852cd6'
+                                    inputPlaceholder='Enter a keyword...'
+                                    searchInNewPage={true}
+                                    pageUrl={`${new URL(`${process.env.ENV_SiteUrl}/search`)}`}
+                                    queryPathBehavior={QueryPathBehavior.QueryParameter}
+                                    queryStringParameter='k'
+                                    defaultQueryStringParameter='k'
+                                    openBehavior={PageOpenBehavior.Self}
+                                />
 
-                <SearchInputWebComponent 
-                    id='f566fb98-4010-485b-a44b-b53c16852cd6'
-                    inputPlaceholder='Enter a keyword...'
-                    searchInNewPage={true}
-                    pageUrl={`${new URL(`${process.env.ENV_SiteUrl}/search`)}`}
-                    queryPathBehavior={QueryPathBehavior.QueryParameter}
-                    queryStringParameter='k'
-                    defaultQueryStringParameter='k'
-                    openBehavior={PageOpenBehavior.Self}
-                />
-
-                <MgtLoginWebComponent loginView='avatar'></MgtLoginWebComponent>                
+                                <MgtLoginWebComponent loginView='avatar'></MgtLoginWebComponent>
+                            </>;
+                }}   
+                </BrowserOnly>                
             </div>;
 }
